e7ac2102aaab12589101972eebe128284743e6b5
Eileen.Zhang <eileen.heyer@icloud.com>
2026-04-14 18:31:27 +0200

    top_skills

--- sql_project1/1_top_paying_jobs.sql
+++ sql_project1/1_top_paying_jobs.sql
@@ -11,8 +11,15 @@ select
       job_location,
       job_schedule_type,
       salary_year_avg,
-      job_posted_date
+      job_posted_date,
+      name as company_name
 from 
-    job_postingss_fact
-
- 
+    job_postings_fact
+left join company_dim on job_postings_fact.company_id = company_dim.company_id
+where 
+    job_title_short ='Data Analyst' AND
+    job_location='Anywhere' AND
+    salary_year_avg is not null
+order by 
+    salary_year_avg desc
+limit 10;
