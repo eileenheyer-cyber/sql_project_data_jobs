1d117c1bca79da29f7add815349383110e1ae7f2
Eileen.Zhang <eileen.heyer@icloud.com>
2026-04-14 23:04:59 +0200

    skills_demand

--- sql_project1/3_top_demand_skills.sql
+++ sql_project1/3_top_demand_skills.sql
@@ -1,4 +1,20 @@
-SELECT *
+
+/*question;what are the most in demand skills for data analyst?
+
+
+*/
+
+SELECT 
+skills,
+count(skills_job_dim.job_id) as skills_demand
 from job_postings_fact
 inner join skills_job_dim on job_postings_fact.job_id = skills_job_dim.job_id
 inner join  skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
+where 
+   job_title_short ='Data Analyst' AND
+   job_location= 'Munich'
+group by 
+   skills
+order by 
+   skills_demand DESC
+limit 5
